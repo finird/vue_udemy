81b97beab2f538b14a8a9ee671c3a8c324fcc5ba
Truong <truongdl@dgroup.co>
2022-08-20 12:19:49 +0700

    embedded html interpolation

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,15 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
-      vueLink: 'https://vuejs.org'
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "<h1>Embedded HTML</h1>",
+      vueLink: "https://vuejs.org",
     };
   },
   methods: {
     outputGoal() {
-      return 'Master Vue!';
-    }
-  }
+      return "Master Vue!";
+    },
+  },
 });
 
-app.mount('#user-goal');
+app.mount("#user-goal");
