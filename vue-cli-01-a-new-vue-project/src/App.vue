62411aa213d6c5b1fd8ac46ed6c7e34ae7c3a0ae
finird <dltruong.c@gmail.com>
2022-08-27 17:44:02 +0700

    add styling

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>Hello, Vue!</h1>
-  <friend-details v-for="friend in friends" :key="friend.id" :friend="friend" />
+  <header>
+    <h1>Hello, Vue!</h1>
+  </header>
+  <ul>
+    <friend-details v-for="friend in friends" :key="friend.id" :friend="friend" />
+  </ul>
 </template>
 
 <script>
@@ -29,23 +33,69 @@ export default {
 </script>
 
 <style>
+
 * {
-  margin: 0;
-  padding: 0;
   box-sizing: border-box;
 }
 
 html {
-  font-size: 16px;
+  font-family: 'Jost', sans-serif;
 }
 
 body {
-  font-family: sans-serif;
+  margin: 0;
 }
 
-h1 {
-  font-size: 2rem;
-  font-weight: bold;
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
   text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
 </style>
