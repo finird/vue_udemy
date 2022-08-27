619585249d1990f6ec0c7ed43c83e77913cefc99
finird <dltruong.c@gmail.com>
2022-08-27 09:43:27 +0700

    vue templates

--- behind-scenes-01-starting-setup/app2.js
+++ behind-scenes-01-starting-setup/app2.js
@@ -1,4 +1,8 @@
 Vue.createApp({
+  template: `
+    <button @click="changeMessage">Update message</button>
+    <p>{{ message }}</p>
+  `,
   data() {
     return {
       message: "Another Vue app instance!",
